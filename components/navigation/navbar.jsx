ea0f3769fd7a01f9c94ebea59190cd6adfbe2acf
Aaron Vasquez <you@example.com>
2024-07-15 11:58:10 -0600

    Header con Dropdowns Mejorados

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -5,13 +5,15 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import { faBolt, faArrowDown, faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, } from "@nextui-org/dropdown";
 import { Avatar } from "@nextui-org/avatar";
 
 export default function NavbarApp() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
     const [isMobileScreen, setIsMobileScreen] = React.useState(false);
+    const [isInvDropdownOpen, setIsInvDropdownOpen] = React.useState(false);
+    const [isAdmDropdownOpen, setIsAdmDropdownOpen] = React.useState(false);
 
     useEffect(() => {
         const handleResize = () => {
@@ -33,6 +35,14 @@ export default function NavbarApp() {
         "Contacto",
 
     ];
+
+    const handleAdminOpenChange = (isAdmDropdownOpen) => {
+        setIsAdmDropdownOpen(isAdmDropdownOpen);
+    }
+
+    const handleInvOpenChange = (isInvDropdownOpen) => {
+        setIsInvDropdownOpen(isInvDropdownOpen);
+    }
 
     return (
         <Navbar
@@ -79,14 +89,23 @@ export default function NavbarApp() {
                     </NavbarItem>
                     <NavbarItem className="flex self-center ml-2">
                         <Dropdown
+
+                            isOpen={isAdmDropdownOpen}
+                            placement=""
+                            onOpenChange={handleAdminOpenChange}
+
                         >
                             <DropdownTrigger>
                                 <Button
 
                                     variant="flat"
-                                    className="bg-main-orange font-semibold rounded-lg "
+                                    className="bg-main-orange font-semibold rounded-xl "
                                 >
                                     Admin
+                                    <FontAwesomeIcon
+                                        icon={faCaretDown}
+                                        className={`ml-1 transition-transform duration-200 ${isAdmDropdownOpen ? 'rotate-180' : ''}`}
+                                    />
                                 </Button>
                             </DropdownTrigger>
                             <DropdownMenu
@@ -98,13 +117,44 @@ export default function NavbarApp() {
 
                                 }}
                                 aria-label="Static Actions">
-                                <DropdownItem className="transition duration-200 ease-in-out" key="new">Gestion de Proyectos</DropdownItem>
-                                <DropdownItem className="transition duration-200 ease-in-out" key="copy">Gestion de Inventario</DropdownItem>
-                                <DropdownItem className="transition duration-200 ease-in-out" key="edit">Gestion Financiera</DropdownItem>
-                                <DropdownItem className="transition duration-200 ease-in-out" key="edit">Reportes y Analisis</DropdownItem>
-                                <DropdownItem className="transition duration-200 ease-in-out" key="delete" color="danger">
-                                    Delete file
-                                </DropdownItem>
+                                <DropdownItem className="transition duration-200 ease-in-out" key="proyectos">Gestion de Proyectos</DropdownItem>
+                                <DropdownItem className="transition duration-200 ease-in-out" key="finanzas">Gestion Financiera</DropdownItem>
+                                <DropdownItem className="transition duration-200 ease-in-out" key="reportes">Reportes y Analisis</DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                    </NavbarItem>
+                    <NavbarItem className="flex self-center ml-2">
+                        <Dropdown
+                            isOpen={isInvDropdownOpen}
+                            onOpenChange={handleInvOpenChange}
+                        >
+                            <DropdownTrigger
+
+                            >
+                                <Button
+                                
+                                    variant="flat"
+                                    className="bg-blue-600 text-white font-semibold rounded-xl "
+                                >
+                                    Gestion de Inventarios
+                                    <FontAwesomeIcon icon={faCaretDown}
+                                    className={`ml-1 transition-transform duration-200 ${isInvDropdownOpen ? 'rotate-180' : ''}`} />
+
+
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu
+                                className=""
+                                color="warning"
+                                style={{
+                                    backgroundColor: "#171717",
+                                    borderRadius: "8px",
+
+                                }}
+                                aria-label="Static Actions">
+                                <DropdownItem className="transition duration-200 ease-in-out" key="equipos"> Equipos</DropdownItem>
+                                <DropdownItem className="transition duration-200 ease-in-out" key="materiales"> Materiales</DropdownItem>
+
                             </DropdownMenu>
                         </Dropdown>
                     </NavbarItem>
@@ -176,7 +226,7 @@ export default function NavbarApp() {
                 <NavbarItem className="flex flex-col items-center mt-6 mb-2">
                     <Dropdown placement="bottom">
                         <DropdownTrigger>
-                            
+
                             <Avatar
                                 size="lg"
                                 isBordered
@@ -186,7 +236,7 @@ export default function NavbarApp() {
 
                                 src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
                             />
-                        
+
                         </DropdownTrigger>
                         <DropdownMenu
                             aria-label="Profile Actions"
@@ -206,15 +256,6 @@ export default function NavbarApp() {
                             <DropdownItem key="settings">
                                 My Settings
                             </DropdownItem>
-                            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                            <DropdownItem key="analytics">
-                                Analytics
-                            </DropdownItem>
-                            <DropdownItem key="system">System</DropdownItem>
-                            <DropdownItem key="configurations">Configurations</DropdownItem>
-                            <DropdownItem key="help_and_feedback">
-                                Help & Feedback
-                            </DropdownItem>
                             <DropdownItem key="logout" color="danger">
                                 Log Out
                             </DropdownItem>
@@ -223,7 +264,7 @@ export default function NavbarApp() {
                 </NavbarItem>
                 <NavbarItem className="flex flex-col items-center justify-center mb-6">Perfil</NavbarItem>
                 {menuItems.map((item, index) => (
-                    <NavbarMenuItem  key={`${item}-${index}`}>
+                    <NavbarMenuItem key={`${item}-${index}`}>
                         <Link
                             className="flex flex-col  items-center justify-center py-2 px-3 text-lg font-semibold hover:text-main-orange transition duration-300 ease-in-out"
                             color={
@@ -239,18 +280,25 @@ export default function NavbarApp() {
 
                 <NavbarMenuItem className="flex flex-col items-center justify-center">
                     <Dropdown
+                        isOpen={isAdmDropdownOpen}
+                        placement=""
+                        onOpenChange={handleAdminOpenChange}
                     >
                         <DropdownTrigger>
                             <Button
-
                                 variant="flat"
-                                className="bg-main-orange font-semibold rounded-lg "
+                                className="bg-main-orange text-black w-1/2 h-14  font-semibold rounded-xl "
                             >
                                 Admin
+                                <FontAwesomeIcon
+                                    icon={faCaretDown}
+                                    className={`ml-1 transition-transform duration-200 ${isAdmDropdownOpen ? 'rotate-180' : ''}`}
+                                />
                             </Button>
                         </DropdownTrigger>
                         <DropdownMenu
                             className=""
+
                             color="warning"
                             style={{
                                 backgroundColor: "#171717",
@@ -258,17 +306,48 @@ export default function NavbarApp() {
 
                             }}
                             aria-label="Static Actions">
-                            <DropdownItem className="transition duration-200 ease-in-out" key="new">Gestion de Proyectos</DropdownItem>
-                            <DropdownItem className="transition duration-200 ease-in-out" key="copy">Gestion de Inventario</DropdownItem>
-                            <DropdownItem className="transition duration-200 ease-in-out" key="edit">Gestion Financiera</DropdownItem>
-                            <DropdownItem className="transition duration-200 ease-in-out" key="edit">Reportes y Analisis</DropdownItem>
-                            <DropdownItem className="transition duration-200 ease-in-out" key="delete" color="danger">
-                                Delete file
-                            </DropdownItem>
+                            <DropdownItem className="transition duration-200 ease-in-out" key="equipos">Gestion de Proyectos</DropdownItem>
+                            <DropdownItem className="transition duration-200 ease-in-out" key="finanzas">Gestion Financiera</DropdownItem>
+                            <DropdownItem className="transition duration-200 ease-in-out" key="reportes">Reportes y Analisis</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavbarMenuItem>
+                <NavbarMenuItem className="flex flex-col items-center mt-3 justify-center">
+                    <Dropdown
+                        isOpen={isInvDropdownOpen}
+                        onOpenChange={handleInvOpenChange}
+                    >
+                        <DropdownTrigger
+                        >
+                            <Button
+                                variant="flat"
+                                className="bg-blue-600  h-14 px-2  text-white font-semibold rounded-xl "
+                            >
+                                Gestion de Inventarios
+                                <FontAwesomeIcon icon={faCaretDown}
+                                    className={`ml-1 transition-transform duration-200 ${isInvDropdownOpen ? 'rotate-180' : ''}`} />
+
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu
+                            className="w-full "
+                            color="warning"
+
+                            style={{
+                                backgroundColor: "#171717",
+                                borderRadius: "8px",
+
+
+                            }}
+                            aria-label="Static Actions">
+                            <DropdownItem className="transition duration-200 ease-in-out" key="equipos"> Equipos</DropdownItem>
+                            <DropdownItem className="transition duration-200 ease-in-out" key="materiales"> Materiales</DropdownItem>
+
                         </DropdownMenu>
                     </Dropdown>
                 </NavbarMenuItem>
             </NavbarMenu>
+
         </Navbar>
     )
 }
