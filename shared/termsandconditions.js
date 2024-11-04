8cb59462ba7535cb9e98102d07ce8a442a6071cd
AdriiViquez <101229435+AdriiViquez@users.noreply.github.com>
2024-11-04 12:20:25 -0600

    Pagina Terminos y Condiciones
    Pagina completa de terminos y condiciones

---
+++ shared/termsandconditions.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        tittle: "1. Aceptación de los Términos",
+        content: "Al acceder y utilizar nuestro sitio web o servicios, aceptas estar sujeto a estos Términos y Condiciones y a nuestra Política de Privacidad. Si no estás de acuerdo con alguno de estos términos, te pedimos que no utilices nuestros Servicios."
+      },
+      {
+        tittle: "2. Uso de los Servicios",
+        content: "Debes ser mayor de edad o contar con el consentimiento de un tutor legal para utilizar nuestros Servicios. Al acceder, declaras que cumples con este requisito. Nos reservamos el derecho de limitar o denegar el acceso a los Servicios en cualquier momento y por cualquier razón, incluyendo el incumplimiento de estos Términos y Condiciones."
+      },
+      {
+        tittle: "3. Registro y Cuenta de Usuario",
+        content: "Para acceder a ciertas funcionalidades, puedes necesitar crear una cuenta con nosotros. Eres responsable de mantener la confidencialidad de tus credenciales de acceso y de las actividades que realices en tu cuenta. Notifícanos de inmediato en caso de uso no autorizado de tu cuenta."
+      },
+      {
+        tittle: "4. Privacidad",
+        content: "Tu privacidad es importante para nosotros. Consulta nuestra Política de Privacidad para obtener información detallada sobre cómo recopilamos, usamos y protegemos tus datos personales."
+      },
+      {
+        tittle: "5. Propiedad Intelectual",
+        content: "Todo el contenido disponible en nuestros Servicios, incluyendo textos, gráficos, logos, íconos, imágenes, videos y software, es propiedad de Sistema Electricidad Total o de sus licenciantes y está protegido por leyes de propiedad intelectual. No puedes usar, modificar, reproducir, distribuir o almacenar este contenido sin nuestro permiso previo por escrito."
+      },
+      {
+        tittle: "6. Conducta Prohibida",
+        content: "Al usar nuestros Servicios, aceptas no: Violar leyes o regulaciones aplicables, hacer un mal uso de la información de otros usuarios, suplantar a cualquier persona o entidad, usar nuestros Servicios para fines comerciales sin autorización previa, o realizar cualquier actividad que interfiera con el funcionamiento adecuado de los Servicios."
+      },
+      {
+        tittle: "7. Limitación de Responsabilidad",
+        content: "Sistema de Electricidad Total no se hace responsable por daños directos, indirectos, incidentales o consecuentes que puedan surgir del uso de nuestros Servicios, incluyendo, sin limitación, pérdidas económicas, pérdida de datos, interrupciones del servicio o problemas de conectividad."
+      },
+      {
+        tittle: "8. Enlaces a Sitios de Terceros",
+        content: "Nuestros Servicios pueden contener enlaces a sitios web o servicios de terceros. No tenemos control sobre estos sitios y no asumimos responsabilidad alguna por su contenido o prácticas. Te recomendamos leer los términos y condiciones de estos sitios antes de usarlos."
+      },
+      {
+        tittle: "9. Modificaciones",
+        content: "Nos reservamos el derecho de modificar o actualizar estos Términos y Condiciones en cualquier momento. Notificaremos dichos cambios a través de nuestro sitio web o mediante otros métodos apropiados. Al continuar utilizando nuestros Servicios, aceptas los términos actualizados."
+      },
+      {
+        tittle: "10. Terminación",
+        content: "Podemos suspender o terminar tu acceso a los Servicios en cualquier momento y sin previo aviso, especialmente si incumples estos Términos y Condiciones."
+      },
+      {
+        tittle: "11. Ley Aplicable",
+        content: "Estos Términos y Condiciones se regirán e interpretarán de acuerdo con las leyes de Costa Rica, sin tener en cuenta sus disposiciones sobre conflictos de leyes."
+      }
+]
